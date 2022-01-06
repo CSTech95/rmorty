6e50a01d3be843a1a59a5d6b2dd0be2780876c4b
Aser Docteur <aser@Asers-MacBook-Pro.local>
2022-01-06 14:40:33 -0500

    Minor Tweaks

--- rmorty-client/src/components/Home.js
+++ rmorty-client/src/components/Home.js
@@ -14,7 +14,7 @@ export default function Home() {
             </div>
         </div>
         <br/>
-            <div className="flex justify-center text-center text-white m-4 p-3 text-3xl shadow-lg shadow-indigo-500/50 bg-black">
+            <div className="flex justify-center text-center text-white my-4 p-3 text-3xl shadow-lg shadow-indigo-500/50 bg-black">
                 <h1>Sign up or Log in to view characters you've save. 
                     After you've logged in, refresh the page and head over to your dashboard.</h1>
             </div>
