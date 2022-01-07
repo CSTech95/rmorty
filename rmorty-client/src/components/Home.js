0a78ceca16b60d23f5866f256d7d955804f98d9c
Aser Docteur <aser@Asers-MacBook-Pro.local>
2022-01-06 21:08:56 -0500

    Minor Tweaks

--- rmorty-client/src/components/Home.js
+++ rmorty-client/src/components/Home.js
@@ -4,8 +4,8 @@ import Signup from "./Signup";
 
 export default function Home() {
     return (
-        <div>     
-        <div className="mt-20 grid gap-4 grid-cols-2 justify-items-center content-center">
+        <div className="bg-gradient-to-bl from-zinc-900 to-blue-900">     
+        <div className=" pt-5 grid gap-4 sm:grid-cols-2 grid-cols-1 justify-items-center content-center">
             <div>
             <Signup/>
             </div>
@@ -15,7 +15,7 @@ export default function Home() {
         </div>
         <br/>
             <div className="flex justify-center text-center text-white my-4 p-3 text-3xl shadow-lg shadow-indigo-500/50 bg-black">
-                <h1>Sign up or Log in to view characters you've save. 
+                <h1>Sign up or Log in to view characters you've saved. 
                     After you've logged in, refresh the page and head over to your dashboard.</h1>
             </div>
         </div>
